4fc453f9e27b1c1bb494b597d4f1ba7dec77b5fe
hussain kadir memon <hussainkadir245@gmail.com>
2026-09-02 19:44:07 +0500

    admin cors error fix in progress

--- index.js
+++ index.js
@@ -9,10 +9,25 @@ import cors from "cors"
 // console.log(config.frontendUrl)
 // middleware
 const app = express();
-app.use(cors({
-    origin: [config.frontendUrl, config.adminFrontendUrl],
-    credentials: true
-}))
+const allowedOrigins = [
+  config.frontendUrl,
+  config.adminFrontendUrl,
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
